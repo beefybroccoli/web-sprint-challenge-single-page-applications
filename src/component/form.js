77a2cd6f1570559defab54638807252ee65bead2
beefybroccoli <tedkimemail@yahoo.com>
2021-09-17 16:33:54 -0700

    added UI

--- src/component/form.js
+++ src/component/form.js
@@ -14,7 +14,22 @@ const Styled_Label = styled.label`
 `;
 
 const Validation_Text = styled.p`
-  background-color: gray;
+  background-color: red;
+`;
+
+const Img_Div = styled.div`
+  background-image: url("https://picsum.photos/500");
+  height: 20vh;
+  width: 100%;
+`;
+
+const Input_Header = styled.div`
+  background-color: rgb(176, 89, 35);
+`;
+
+const Input_Body = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 export default function Form(props) {
   const [stateFormData, set_stateForm] = useState(con.initial_form_state);
@@ -55,87 +70,104 @@ export default function Form(props) {
   }, [stateFormData]);
 
   return (
-    <>
-      <NavigationBar />
+    <div>
+      <h2>Build Your Own Pizza</h2>
+      <Img_Div></Img_Div>
       <form id="pizza-form" onSubmit={cb_onSubmit}>
         <Styled_Label>
-          <b>Name : </b>{" "}
-          <input
-            id="name-input"
-            name="name"
-            type="text"
-            onChange={cb_onChange}
-            value={stateFormData.name}
-          />
-        </Styled_Label>
-        <Validation_Text>{stateValidationText.name}</Validation_Text>
-        {/* ------------------------------------------ */}
-        <Styled_Label>
-          {" "}
-          <b>Size : </b>
-          <select
-            id="size-dropdown"
-            name="size"
-            onChange={cb_onChange}
-            value={stateFormData.size}
-          >
-            <option value="">(please select a size)</option>
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
-          </select>
-        </Styled_Label>
-        <Validation_Text>{stateValidationText.size}</Validation_Text>
-        {/* ------------------------------------------ */}
+          <Input_Header>
+            <p>Name : </p>
+            <Validation_Text>{stateValidationText.name}</Validation_Text>
+          </Input_Header>
 
-        <Styled_Label>
-          <b>Cheese</b>
-          <input
-            type="checkbox"
-            name="topping_cheese"
-            onChange={cb_onChange}
-            value={stateFormData.topping_cheese}
-          />
+          <Input_Body>
+            <input
+              id="name-input"
+              name="name"
+              type="text"
+              onChange={cb_onChange}
+              value={stateFormData.name}
+            />
+          </Input_Body>
         </Styled_Label>
 
         {/* ------------------------------------------ */}
         <Styled_Label>
-          <b>Pie</b>
-          <input
-            type="checkbox"
-            name="topping_pie"
-            onChange={cb_onChange}
-            value={stateFormData.topping_pie}
-          />
-        </Styled_Label>
-
-        {/* ------------------------------------------ */}
-        <Styled_Label>
-          <b>Vegetable</b>
-          <input
-            type="checkbox"
-            name="topping_vegetable"
-            onChange={cb_onChange}
-            value={stateFormData.topping_vegetable}
-          />
-        </Styled_Label>
-
-        {/* ------------------------------------------ */}
-
-        <Styled_Label>
-          <b>Chili</b>
-          <input
-            type="checkbox"
-            name="topping_chili"
-            onChange={cb_onChange}
-            value={stateFormData.topping_chili}
-          />
+          <Input_Header>
+            <p>Size : </p>
+            <Validation_Text>{stateValidationText.size}</Validation_Text>
+          </Input_Header>
+          <Input_Body>
+            <select
+              id="size-dropdown"
+              name="size"
+              onChange={cb_onChange}
+              value={stateFormData.size}
+            >
+              <option value="">(please select a size)</option>
+              <option value="small">Small</option>
+              <option value="medium">Medium</option>
+              <option value="large">Large</option>
+            </select>
+          </Input_Body>
         </Styled_Label>
 
         {/* ------------------------------------------ */}
 
+        <Input_Header>Add Toppings</Input_Header>
+        <Input_Body>
+          <Styled_Label>
+            <b>Cheese</b>
+            <input
+              type="checkbox"
+              name="topping_cheese"
+              onChange={cb_onChange}
+              value={stateFormData.topping_cheese}
+            />
+          </Styled_Label>
+
+          {/* ------------------------------------------ */}
+          <Styled_Label>
+            <b>Pie</b>
+            <input
+              type="checkbox"
+              name="topping_pie"
+              onChange={cb_onChange}
+              value={stateFormData.topping_pie}
+            />
+          </Styled_Label>
+
+          {/* ------------------------------------------ */}
+          <Styled_Label>
+            <b>Vegetable</b>
+            <input
+              type="checkbox"
+              name="topping_vegetable"
+              onChange={cb_onChange}
+              value={stateFormData.topping_vegetable}
+            />
+          </Styled_Label>
+
+          {/* ------------------------------------------ */}
+
+          <Styled_Label>
+            <b>Chili</b>
+            <input
+              type="checkbox"
+              name="topping_chili"
+              onChange={cb_onChange}
+              value={stateFormData.topping_chili}
+            />
+          </Styled_Label>
+        </Input_Body>
+        {/* ------------------------------------------ */}
+
         <Styled_Label>
-          <b>Special Instruction : </b>{" "}
+          <Input_Header>
+            <p>Special Instruction : </p>
+          </Input_Header>
+          <Input_Body></Input_Body>
+
           <input
             id="special-text"
             name="special_instruction"
@@ -146,14 +178,17 @@ export default function Form(props) {
         </Styled_Label>
 
         {/* ------------------------------------------ */}
-        <button
-          name="submit_button"
-          id="order-button"
-          disabled={!stateFormValidation}
-        >
-          Submit
-        </button>
+        <Input_Header>Sub Order</Input_Header>
+        <Input_Body>
+          <button
+            name="submit_button"
+            id="order-button"
+            disabled={!stateFormValidation}
+          >
+            Submit
+          </button>
+        </Input_Body>
       </form>
-    </>
+    </div>
   );
 }
